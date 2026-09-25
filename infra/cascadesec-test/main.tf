c152fd0a9faee74ab4ebddddd15f305be3306730
konradkelly <konradky@gmail.com>
2026-09-25 10:53:58 -0700

    CascadeSec v3 test fixture (do not merge)

---
+++ infra/cascadesec-test/main.tf
@@ -0,0 +1,19 @@
+# CascadeSec v3 test fixture -- deliberately misconfigured. This PR is a test
+# of the CascadeSec GitHub App and will be closed without merging.
+
+resource "aws_s3_bucket" "test_artifacts" {
+  bucket = "pugetscope-cascadesec-test-artifacts"
+}
+
+resource "aws_security_group" "test_bastion" {
+  name        = "cascadesec-test-bastion"
+  description = "Test bastion"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
